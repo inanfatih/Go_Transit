671968357e90294f290d3be68c8f3bba919aa73b
fatih <mfatihinan@gmail.com>
2018-01-11 00:11:14 -0500

    first page is completed

---
+++ app/src/main/java/com/example/fatih/gotransit/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.fatih.gotransit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
