9331c5b4c87515030d06fff844e333b94cb1dece
fatih <mfatihinan@gmail.com>
2018-01-11 03:28:43 -0500

    popup on the last screen is missing

--- app/src/main/java/com/example/fatih/gotransit/CarpoolCreate.java
+++ app/src/main/java/com/example/fatih/gotransit/CarpoolCreate.java
@@ -1,7 +1,9 @@
 package com.example.fatih.gotransit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class CarpoolCreate extends AppCompatActivity {
@@ -15,6 +17,12 @@ public class CarpoolCreate extends AppCompatActivity {
         ImageView imageView;
         imageView = findViewById(R.id.imageView3);
         imageView.bringToFront();
+
+    }
+
+    public void createClicked(View view) {
+        Intent intent = new Intent(this, Confirmation.class);
+        startActivity(intent);
 
     }
 }
